a74a4b9c2a533ded10871c79d00ddf41a8ac0bc2
Doug Hamilton <Dmhamilton1@live.com>
2024-03-02 17:13:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const functions = require('firebase-functions');
+const axios = require('axios');
+
+exports.processNfcScan = functions.https.onRequest(async (req, res) => {
+  const { name, email } = req.body;
+  const apiKey = 'YOUR_MAILCHIMP_API_KEY';
+  const listId = 'YOUR_MAILCHIMP_LIST_ID';
+
+  const data = {
+    email_address: email,
+    status: 'subscribed',
+    merge_fields: {
+      FNAME: name,
+    },
+  };
+
+  try {
+    const response = await axios.post(`https://us12.api.mailchimp.com/3.0/lists/${listId}/members`, data, {
+      headers: {
+        'Authorization': `Basic ${Buffer.from('anystring:' + apiKey).toString('base64')}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    res.status(200).send('Success');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error');
+  }
+});
 
