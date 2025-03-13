416edadff3dbe7f94c7aa0affe28b334812855f5
Doug Hamilton <Dmhamilton1@live.com>
2025-03-13 10:22:19 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new Dotenv()
+  ]
+};
