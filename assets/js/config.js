8e1ae10577941674db2854e05cab7c75238200ec
Doug Hamilton <Dmhamilton1@live.com>
2025-07-25 12:42:21 -0400

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,35 @@
+// assets/js/config.js
+
+export const CONFIG = {
+  cardImages: [
+    'images/yellowscullcard.png',
+    'images/redscullcard.png',
+    'images/Descartes.png',
+    'images/Alexandor.png',
+    'images/Medusa.png',
+    'images/Elara.png',
+    'images/adventurercard.png',
+    'images/collaboratorcard.png',
+    'images/humanitariancard.png',
+    'images/nextcard.png',
+    'images/Aegis.png',
+    'images/resilientcard.png',
+    'images/Astrid.png',
+    'images/nicolecard.png'
+  ],
+  videos: [
+    'images/Descartesvideo.mp4',
+    'images/medusa best.mp4',
+    'images/medusa 1.mp4',
+    'images/young medusa.mp4',
+    'images/Alexandorvideo.mp4',
+    'images/Elaravideo.mp4',
+    'images/elara rage.mp4',
+    'images/astridvideo.mp4',
+    'images/aegisvideo.mp4',
+    'images/wolfvideo.mp4',
+    'images/merlin.mp4',
+    'images/hermesvideo.mp4',
+    'images/Mystery.mp4'
+  ]
+};
