505226a0078f5041775341d96c10b8ba13913765
Doug Hamilton <Dmhamilton1@live.com>
2025-07-27 02:07:07 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'charlestonhacks-v57'; // update this on each deploy
+const cacheName = 'charlestonhacks-v58'; // update this on each deploy
 const assetsToCache = [
   '/',
   '/index.html',
