7435936a214345529fd827e27fad238d7b6e701a
Doug Hamilton <Dmhamilton1@live.com>
2025-07-29 13:13:34 -0400

    Update neuralInteractive.js

--- assets/js/neuralInteractive.js
+++ assets/js/neuralInteractive.js
@@ -7,6 +7,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = 'https://hvmotpzhliufzomewzfl.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bW90cHpobGl1ZnpvbWV3emZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NzY2NDUsImV4cCI6MjA1ODE1MjY0NX0.foHTGZVtRjFvxzDfMf1dpp0Zw4XFfD-FPZK-zRnjc6s';
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabase = supabase; // Add this line to expose supabase for debugging
 
 // ---- NEURON "TEMPLATE" ----
 // These are the default neurons for a brand new user.
