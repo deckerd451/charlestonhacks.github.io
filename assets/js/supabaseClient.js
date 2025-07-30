a590b4c65a8c0b3dce0a7d0af47fb2548b437ca0
Doug Hamilton <Dmhamilton1@live.com>
2025-07-30 10:43:26 -0400

    Update supabaseClient.js

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const SUPABASE_URL = 'https://hvmotpzhliufzomewzfl.supabase.co';
-export const SUPABASE_KEY = 'YOUR_SUPABASE_KEY_HERE';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bW90cHpobGl1ZnpvbWV3emZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NzY2NDUsImV4cCI6MjA1ODE1MjY0NX0.foHTGZVtRjFvxzDfMf1dpp0Zw4XFfD-FPZK-zRnjc6s';;
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
