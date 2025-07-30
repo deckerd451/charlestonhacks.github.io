0e03cc4bc5149e48ae934f2454bb054d5bb6bf57
Doug Hamilton <Dmhamilton1@live.com>
2025-07-30 11:26:02 -0400

    Update state.js

--- assets/js/state.js
+++ assets/js/state.js
@@ -1,10 +1,8 @@
 // src/state.js
 
-// Supabase Configuration (keep as is)
-export const SUPABASE_URL = 'https://hvmotpzhliufzomewzfl.supabase.co';
-export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bW90cHpobGl1ZnpvbWV3emZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1NzY2NDUsImV4cCI6MjA1ODE1MjY0NX0.foHTGZVtRjFvxzDfMf1dpp0Zw4XFfD-FPZK-zRnjc6s';
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+import { supabaseClient as supabase } from './supabaseClient.js';
+export { supabase };
+
 
 // Global App State
 export const appState = {
