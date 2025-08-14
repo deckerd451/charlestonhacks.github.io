95f1f9a7d79e39a6c2fd343865585557a550189b
Doug Hamilton <Dmhamilton1@live.com>
2025-08-14 17:15:58 -0400

    Create domElements.js

---
+++ assets/domElements.js
@@ -0,0 +1,15 @@
+// /assets/js/domElements.js
+export const dom = {
+  // Search tab
+  cardContainer: document.getElementById('cardContainer'),
+  matchNotification: document.getElementById('matchNotification'),
+  noResults: document.getElementById('noResults'),
+
+  // Team tab
+  bestTeamContainer: document.getElementById('bestTeamContainer'),
+
+  // Leaderboard tab
+  leaderboardRows: document.getElementById('leaderboard-rows'),
+};
+
+export default dom;
